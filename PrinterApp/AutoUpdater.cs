bc6cfaf915efcf1be2fce4ca858c06444304e5b7
EvgeniyD <dyakov.hellvisionstudio@gmail.com>
2023-12-24 19:24:20 +0500

    change update time

--- PrinterApp/AutoUpdater.cs
+++ PrinterApp/AutoUpdater.cs
@@ -215,7 +215,7 @@ public class AutoUpdater
     private const string BatFile = @"@echo off
 echo start
 setlocal
-timeout 5
+timeout 61
 for /f ""tokens=2 delims=="" %%a in ('wmic OS Get localdatetime /value') do set ""dt=%%a""
 set ""YY=%dt:~2,2%"" & set ""YYYY=%dt:~0,4%"" & set ""MM=%dt:~4,2%"" & set ""DD=%dt:~6,2%""
 set ""HH=%dt:~8,2%"" & set ""Min=%dt:~10,2%"" & set ""Sec=%dt:~12,2%""
